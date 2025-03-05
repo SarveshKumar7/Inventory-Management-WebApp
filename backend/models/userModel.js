6efdb94e27cc0b0c99efb8f1ca075039fa83e5d3
Sarvesh Kumar <140077187+SarveshKumar7@users.noreply.github.com>
2025-03-05 22:18:04 +0530

    user-model

---
+++ backend/models/userModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose")
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please add a name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please add a email"],
+        unique: true,
+        trim: true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,"Please enter a valid email"
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"],
+        minLength: [6, "Password must be upto 6 characters"],
+        maxLength: [23, "Password must not be more than 23 characters"],
+    },
+    photo: {
+        type: String,
+        required: [true, "Please add a photo"],
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7ON0Qbmq_1dlIIgwWOsLNe9j5Bfjt3NM3jQ&s"
+    },
+    phone: {
+        type: String,
+        default: "+91"
+    },
+    bio: {
+        type: String,
+        maxLength: [250, "Bio must not be more than 250 Characters"],
+        default: "bio"
+    }
+},{
+    timestamps:true,
+})
+
+const user = mongoose.model("User", userSchema)
+module.exports =User
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,17 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const app = express()
+const app = express();
+
+//Middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//routes
+app.get("/",(req, res)=>{
+    res.send("Home Page");
+})
 
 const PORT = process.env.PORT || 5000;
 

345dda37f32865e062d52aba490539d3f18d3c87
Sarvesh Kumar <140077187+SarveshKumar7@users.noreply.github.com>
2025-03-12 09:22:28 +0530

    Protect-Middleware and UserProfile Controller(Backend)

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {registerUser, LoginUser, logout} = require("../controllers/userController");
+const {registerUser, LoginUser, logout, getUser} = require("../controllers/userController");
+const protect = require("../middleWare/authMiddleware");
 
 
 router.post("/register", registerUser);
 router.post("/login", LoginUser);
 router.get("/logout", logout);
+router.get("/getuser", protect, getUser);
 
 module.exports = router;
